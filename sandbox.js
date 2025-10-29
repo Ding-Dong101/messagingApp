e443669094227637ecf91df133bbcd389d474641
Ding-Dong101 <kwawerastus2@gmail.com>
2025-10-29 11:08:46 +0000

    update on the interface

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,26 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-db.collection('messages').get().then((snapshot) =>{
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data())
+db.collection("messages")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      console.log(doc.data());
     });
-})
+  });
 
-form.addEventListener('submit', e =>{
-    e.preventDefault();
-    try{
-        db.collection('messages').add({
-            name: form.name.value,
-            friend:form.friendName.value,
-            message:form.msg.value 
-        })
-//         
-    }catch(err){
-        alert(err)
-    }finally{
-        form.name.value = '';
-        form.friendName.value = '';
-        form.msg.value = 'Your Message Is sent';
-    }
-   
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  try {
+    db.collection("messages").add({
+      name: form.name.value,
+      message: form.msg.value,
+    });
+    //
+  } catch (err) {
+    alert(err);
+  } finally {
+    form.name.value = "";
+    form.friendName.value = "";
+    form.msg.value = "Your Message Is sent";
+  }
+});
